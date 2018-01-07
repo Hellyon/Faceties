e066f97480fe93491acd9a16b6c4749f269d82c3
Tanguy Barroso <tanguy.barroso@etu.uca.fr>
2018-01-07 18:32:38 +0100

    préparation KullbackLeibler

---
+++ KullbackLeibler.m
@@ -0,0 +1,4 @@
+#Fonction de calcul grâce à la distance de Bhattacharrya
+function divergence = KullbackLeibler(H1,H2)
+  
+endfunction
--- ReconnaissanceEuclidienne.m
+++ ReconnaissanceEuclidienne.m
@@ -1,5 +1,5 @@
 #Appels des fonctions pour la reconnaissance grâce la distance Euclidienne puis l'affichage des visages les plus proches
 function ReconnaissanceEuclidienne(M)
-  IDXp = reconEuclidienne(M)
+  IDXp = reconEuclidienne(M);
   affichageResultats(M, IDXp);
 endfunction
